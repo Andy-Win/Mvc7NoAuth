6625ea3216a98005466772cb3ebc40f38d8456d9
Andy Wang <v-wajian@microsoft.com>
2023-04-10 00:24:02 -0700

    Add project files.

---
+++ Mvc7NoAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mvc7NoAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
